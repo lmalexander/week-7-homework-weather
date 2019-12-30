f6caab440df7dd90b6cd09a69800a89e339eb8e0
lmalexander <elizabeth.alexander@northwestern.edu>
2019-12-29 20:38:14 -0600

    return response object data to html

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 console.log("brand new {code}, city girls going platinum - yung miami");
+const apiKey = "&APPID=1b267613807f3bffd7ce2a8a24e822cd";
 
 // ----------------- current weather forecast
 
@@ -7,18 +8,31 @@ $("#searchButton").on("click" , function (event) {
     event.preventDefault();
     const city = $(".form-control").val();
     console.log(city);
-
 // store query url with user input city
-    let urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=1b267613807f3bffd7ce2a8a24e822cd";
+    let urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + apiKey;
     console.log(urlCurrent);
-
 // ajax call to perform current weather search
     $.ajax ({
         url: urlCurrent,
         method: "GET"
     }).then(function(weatherCurrent) {
         console.log(weatherCurrent);
- //       
+ // create variables with response object data for template literals
+        const currentCity = weatherCurrent.name;
+        const currentTemperature = weatherCurrent.main.temp;
+        const currentHumidity = weatherCurrent.main.humidity;
+        const currentWindSpeed = weatherCurrent.wind.speed;
+ // check variables with console logs
+        console.log(currentCity);
+        console.log(currentTemperature);
+        console.log(currentHumidity);
+        console.log(currentWindSpeed);
+// text variables to html
+        $("#cityName").text(currentCity);
+        $("#currentTemp").text("Temperature: " + currentTemperature + "°") ;
+        $("#currentHum").text("Humidity: " + currentHumidity + "%");
+        $("#currentWS").text("Wind Speed: " + currentWindSpeed + "MPH");
+
 
     })
 
