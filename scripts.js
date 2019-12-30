cf72a3935535a0de9f752717494bd8ee81d94be3
lmalexander <elizabeth.alexander@northwestern.edu>
2019-12-29 19:54:09 -0600

    built user interface, console logged response object

--- scripts.js
+++ scripts.js
@@ -1,17 +1,29 @@
 console.log("brand new {code}, city girls going platinum - yung miami");
 
-// ----------------- current weather forecast api call
+// ----------------- current weather forecast
 
 // get user input
-$("#search").on("click" , function (event) {
-    event.preventDefault;
-    let citySearch = document.querySelector(".form-control");
-    console.log(citySearch.value);
+$("#searchButton").on("click" , function (event) {
+    event.preventDefault();
+    const city = $(".form-control").val();
+    console.log(city);
+
+// store query url with user input city
+    let urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=1b267613807f3bffd7ce2a8a24e822cd";
+    console.log(urlCurrent);
+
+// ajax call to perform current weather search
+    $.ajax ({
+        url: urlCurrent,
+        method: "GET"
+    }).then(function(weatherCurrent) {
+        console.log(weatherCurrent);
+ //       
+
+    })
+
 
 })
 
-    
 
-// do current weather api call
-
-// return current weather data to */ 
+//------------------ five day forecast
