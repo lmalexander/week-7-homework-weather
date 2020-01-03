ca45eb44c2a98803471939832ea9a74d844ccbbc
lmalexander <elizabeth.alexander@northwestern.edu>
2020-01-02 21:38:18 -0600

    console log forecast response data and return function text to html

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
 console.log("brand new {code}, city girls going platinum - yung miami");
 const apiKey = "&APPID=1b267613807f3bffd7ce2a8a24e822cd";
 
-// ----------------- current weather forecast
+// -------------------------------------------- today's date and time
+
+// store date as variable
+const todayDate = moment().format('MMMM Do YYYY');
+const timeNow = moment().format("h:mm a")
+// return variable to html
+$("#todayDate").text("Today is " + todayDate);
+$("#timeNow").text("Current Time: " + timeNow);
+
+// --------------------------------------------- user click event
+$("#searchButton").on("click" , function (event) {
+
+// -------------------- current weather forecast
 
 // get user input
-$("#searchButton").on("click" , function (event) {
     event.preventDefault();
     const city = $(".form-control").val();
     console.log(city);
 // store query url with user input city
-    let urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + apiKey;
+    const urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + apiKey;
     console.log(urlCurrent);
 // ajax call to perform current weather search
     $.ajax ({
@@ -17,7 +28,7 @@ $("#searchButton").on("click" , function (event) {
         method: "GET"
     }).then(function(weatherCurrent) {
         console.log(weatherCurrent);
- // create variables with response object data for template literals
+ // create variables with response object data
         const currentCity = weatherCurrent.name;
         const currentTemperature = weatherCurrent.main.temp;
         const currentHumidity = weatherCurrent.main.humidity;
@@ -32,12 +43,55 @@ $("#searchButton").on("click" , function (event) {
         $("#currentTemp").text("Temperature: " + currentTemperature + "°") ;
         $("#currentHum").text("Humidity: " + currentHumidity + "%");
         $("#currentWS").text("Wind Speed: " + currentWindSpeed + "MPH");
-
-
     })
 
+//----------------------- five day forecast
+// five day url    
+    const urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + apiKey;
+    console.log(urlFiveDay);
+// five day weather search ajax call
+    $.ajax ({
+        url: urlFiveDay,
+        method: "GET"
+    }).then(function(weatherFive) {
+        console.log(weatherFive);
 
+// loop through forecast object
+for (let i = 0; i < weatherFive.list.length; i++) {
+// convert forecast date        
+    const dateRaw = weatherFive.list[i].dt_txt;
+    const forecastDate = moment(dateRaw).format("dddd MMM Do");
+// response object variables
+    const forecastTempMax = weatherFive.list[i].main.temp_max;
+    const forecastTempMin = weatherFive.list[i].main.temp_min;
+    const forecastHum = weatherFive.list[i].main.humidity;
+    const forecastIcon = weatherFive.list[i].weather.icon;
+// check in the console
+    console.log(forecastDate);
+    console.log(forecastTempMax);
+    console.log(forecastTempMin);
+    console.log(forecastHum);
+    console.log(forecastIcon);         
+// create forecast card 
+    const forecastCardNew = function createForecastCard() {
+        $("<div class=card id=forecastCard>");
+        $("<div class=card-body id=forecastCardBody>");
+        $("<div class=card-title id=forecastDate>").text(forecastDate)
+        $("<div class=card-title id=forecastTempMax>").text(forecastTempMax)
+        $("<div class=card-title id=forecastTempMin>").text(forecastTempMin)
+        $("<div class=card-title id=forecastHum>").text(forecastHum)
+        
+    }
+    forecastCardNew;
+// append forecast card to card group
+    document.getElementById("forecastCardGroup").append(forecastCardNew);
+// stop forloop at 5 days
+    if (i > 4) {
+        break;
+    }
+        
+}
+    })
 })
 
 
-//------------------ five day forecast
